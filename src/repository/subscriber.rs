b001c93f2b0db94346200659822c9dd82bfc0ef7
NI04Code <naufalichsan114@gmail.com>
2024-04-15 18:42:48 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -2,7 +2,7 @@ use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 
-//Singleton of Database
+// Singleton of Database
 lazy_static! {
     static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
@@ -14,7 +14,7 @@ impl SubscriberRepository {
         let subscriber_value = subscriber.clone();
         if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
-        };
+        }
 
         SUBSCRIBERS.get(product_type).unwrap()
             .insert(subscriber_value.url.clone(), subscriber_value);
@@ -24,7 +24,7 @@ impl SubscriberRepository {
     pub fn list_all(product_type: &str) -> Vec<Subscriber> {
         if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
-        };
+        }
 
         return SUBSCRIBERS.get(product_type).unwrap().iter()
             .map(|f| f.value().clone()).collect();
